e4f1fa142818d8efc1f9d5f6bd1176ca78f02fce
xiazhuol <xiazhuol@msu.edu>
2018-12-08 21:14:05 -0500

    texture

--- step4/ShaderWnd/vertexSphere2.glsl
+++ step4/ShaderWnd/vertexSphere2.glsl
@@ -1,26 +1,34 @@
 #version 330
 // Input vertex data, different for all executions of this shader.
-layout(location = 0) in vec3 vPosition;
-layout(location = 1) in vec3 vNormal;
-layout(location = 2) in vec2 vTexCoord;
+//layout(location = 0) in vec3 vPosition;
+//layout(location = 1) in vec3 vNormal;
+//layout(location = 2) in vec2 vTexCoord;
 
-out vec3 fN;
-out vec3 fE;
+//out vec3 fN;
+//out vec3 fE;
 
-uniform mat4 mPVM;
-uniform mat4 mVM;
+//uniform mat4 mPVM;
+//uniform mat4 mVM;
 
-void main(){
-   vec4 LightPosition = vec4(-5.f, 5.f, -5.f, 0.f);
+//void main(){
+ //  vec4 LightPosition = vec4(-5.f, 5.f, -5.f, 0.f);
 
    // Transform vertex  position into eye coordinates
-   vec3 pos = (mVM*vec4(vPosition, 1.0)).xyz;
+//   vec3 pos = (mVM*vec4(vPosition, 1.0)).xyz;
 
    // Transform vertex normal into eye coordinates
-   fN = (mVM*vec4(vNormal, 0.0)).xyz;
+//   fN = (mVM*vec4(vNormal, 0.0)).xyz;
         
-   fE = -pos;
+//   fE = -pos;
    
-   gl_Position = mPVM * vec4(vPosition, 1.0);
-}
+//   gl_Position = mPVM * vec4(vPosition, 1.0);
+//}
+attribute vec3 v_coord;
+varying vec4 texCoords;
+uniform mat4 m,v,p;
 
+void main(void) {
+    mat4 mvp = p*v*m;
+    gl_Position = mvp * vec4(v_coord, 1.0);
+    texCoords = vec4(v_coord, 1.0);
+}
